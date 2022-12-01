d989088400023f4b917be30310b588e8c505d7cc
patcoet <saltsvan@gmail.com>
2022-12-01 13:49:03 +0100

    Add Haskell 2022/01
    Add Haskell 2022/01

---
+++ 2022/01/2.hs
@@ -0,0 +1,13 @@
+import Data.List
+import System.Environment
+
+main = do
+  args <- System.Environment.getArgs
+  input <- readFile' args
+  let groups = groupBy (\_ y -> y /= "") $ lines input
+  let filteredGroups = map (filter (/= "")) groups
+  let parsedGroups = map (map read) filteredGroups
+  let sums = map sum parsedGroups
+  print $ sum $ take 3 $ sortBy (flip compare) sums
+  where readFile' [] = readFile "input"
+        readFile' x = readFile $ head x
