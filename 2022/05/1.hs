48f71c5d90468afe07ac2edf53e08fcdc26fb65d
patcoet <saltsvan@gmail.com>
2022-12-05 17:05:50 +0100

    Add Haskell 2022/05 (todo: clean up)

---
+++ 2022/05/1.hs
@@ -0,0 +1,25 @@
+import Data.Char
+import Data.List
+import System.Environment
+
+processInstr stacks (num:from:to:[]) 
+  | from < to = pre ++ [newFrom] ++ mid ++ [newTo] ++ post
+  | otherwise = pre ++ [newTo] ++ mid ++ [newFrom] ++ post
+  where newFrom = drop (num + 1) (stacks !! from)
+        newTo = (reverse $ take (num + 1) (stacks !! from)) ++ stacks !! to
+        sm = min from to
+        la = max from to
+        pre = take sm stacks
+        post = drop (la + 1) stacks
+        mid = take (la - sm - 1) $ drop (sm + 1) stacks
+
+processInstrs stacks (instr:instrs)
+  | instrs == [] = processInstr stacks instr
+  | otherwise = processInstrs (processInstr stacks instr) instrs
+
+main = do
+  args <- System.Environment.getArgs
+  input <- readFile $ head args
+  let stacks = filter (/= "") $ map (filter isLetter) $ transpose $ lines $ takeWhile (not . isDigit) input
+  let instrs = map (map ((\x -> x - 1) . \x -> read x :: Int) . filter ((\x -> not $ elem x ["move", "from", "to"])) . words) $ lines $ dropWhile (/= 'm') input
+  print $ map head $ processInstrs stacks instrs
