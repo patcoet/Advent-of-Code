66404593c49859797ba1c0ff525f062020d6f307
patcoet <saltsvan@gmail.com>
2022-12-06 02:58:19 +0100

    Clean up Haskell 2022/05

--- 2022/05/2.hs
+++ 2022/05/2.hs
@@ -1,17 +1,15 @@
+import Control.Lens.At
+import Control.Lens.Setter
 import Data.Char
 import Data.List
 import System.Environment
 
-processInstr stacks (num:from:to:[]) 
-  | from < to = pre ++ [newFrom] ++ mid ++ [newTo] ++ post
-  | otherwise = pre ++ [newTo] ++ mid ++ [newFrom] ++ post
-  where newFrom = drop (num + 1) (stacks !! from)
-        newTo = (take (num + 1) (stacks !! from)) ++ stacks !! to
-        sm = min from to
-        la = max from to
-        pre = take sm stacks
-        post = drop (la + 1) stacks
-        mid = take (la - sm - 1) $ drop (sm + 1) stacks
+processInstr stacks (num:from:to:[]) = stacks'
+  where
+    from' = from - 1
+    to' = to - 1
+    stacks' = ix from' %~ drop num $ stacks''
+    stacks'' = ix to' %~ (take num (stacks !! from') ++) $ stacks
 
 processInstrs stacks (instr:instrs)
   | instrs == [] = processInstr stacks instr
@@ -19,7 +17,10 @@ processInstrs stacks (instr:instrs)
 
 main = do
   args <- System.Environment.getArgs
-  input <- readFile $ head args
-  let stacks = filter (/= "") $ map (filter isLetter) $ transpose $ lines $ takeWhile (not . isDigit) input
-  let instrs = map (map ((\x -> x - 1) . \x -> read x :: Int) . filter ((\x -> not $ elem x ["move", "from", "to"])) . words) $ lines $ dropWhile (/= 'm') input
+  input <- readFile $ head $ args ++ ["input"]
+  let
+    sPart = lines $ takeWhile (not . isDigit) input
+    stacks = filter (/= "") $ map (filter isLetter) $ transpose sPart
+    iPart = lines $ dropWhile (/= 'm') input
+    instrs = map (map read . filter (any $ not . isLetter) . words) iPart
   print $ map head $ processInstrs stacks instrs
